f22f214bee1f346390f6cb4ff1d2426e12cedfb9
吴才龙 <hengxingyema@163.com>
2019-01-02 17:22:52 +0800

    给洋县用户

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,47 @@
-console.log("Hello LeanEngine!");
+function copyUrl1(){
+    var Url2=document.getElementById("biao1").innerText;
+    var oInput = document.createElement('input');
+    oInput.value = Url2;
+    document.body.appendChild(oInput);
+    oInput.select(); // 选择对象
+    document.execCommand("Copy"); // 执行浏览器复制命令
+    oInput.className = 'oInput';
+    oInput.style.display='none';
+    alert('复制成功');
+}
+
+function copyUrl2(){
+    var Url2=document.getElementById("biao2").innerText;
+    var oInput = document.createElement('input');
+    oInput.value = Url2;
+    document.body.appendChild(oInput);
+    oInput.select(); // 选择对象
+    document.execCommand("Copy"); // 执行浏览器复制命令
+    oInput.className = 'oInput';
+    oInput.style.display='none';
+    alert('复制成功');
+}
+
+function copyUrl3(){
+    var Url2=document.getElementById("biao3").innerText;
+    var oInput = document.createElement('input');
+    oInput.value = Url2;
+    document.body.appendChild(oInput);
+    oInput.select(); // 选择对象
+    document.execCommand("Copy"); // 执行浏览器复制命令
+    oInput.className = 'oInput';
+    oInput.style.display='none';
+    alert('复制成功');
+}
+
+function copyUrl4(){
+    var Url2=document.getElementById("biao4").innerText;
+    var oInput = document.createElement('input');
+    oInput.value = Url2;
+    document.body.appendChild(oInput);
+    oInput.select(); // 选择对象
+    document.execCommand("Copy"); // 执行浏览器复制命令
+    oInput.className = 'oInput';
+    oInput.style.display='none';
+    alert('复制成功');
+}
